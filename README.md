e77f12a1e3a84a598cc963f6a95c4dcf1af44714
kyleisme14 <KyleKallman@gmail.com>
2021-11-10 13:17:37 -0800

    wednesday progression, pretty decent whack=a=mole

--- README.md
+++ README.md
@@ -3,3 +3,22 @@
 This game is a code bootcamp practice excersize. 
 
 It's an in-browser FPS game. 
+
+Instructions: 
+Shoot the baddies as they charge towards you. 
+
+
+WireFrame:
+Final game should look like this.
+
+![Screen Shot 2021-11-08 at 10 34 46 AM](https://user-images.githubusercontent.com/27470736/140798510-23ff8f44-8a1a-464d-ab00-58f2a6dc092b.png)
+
+But first we will do a simple whack-a-mole to get there.
+
+
+![Screen Shot 2021-11-08 at 10 34 50 AM](https://user-images.githubusercontent.com/27470736/140798567-5e97740d-5197-49b3-9977-63e5a95306d3.png)
+
+Upon thinking about the inherent violence of my original idea, I've decided to try something a little different... without changing the name. 
+
+Now: You are a paparazi trying to take photo's of celebrities. You want HEADSHOTS ONLY. 
+
--- main.js
+++ main.js
@@ -1,31 +1,96 @@
-const squares = document.querySelectorAll(".square");
-const mole = document.querySelector(".mole");
-const timeLeft = document.querySelector("#time-left");
-const score = document.querySelector("#score");
-
+let squares = document.querySelectorAll(".square")
+const mole = document.querySelectorAll(".mole")
+let squares1 = document.querySelectorAll(".square1")
+const mole1 = document.querySelectorAll(".mole")
+const timeLeft = document.querySelector("#time-left")
+const score = document.querySelector("#score")
+let timerId = null
 let result = 0
+let hitPosition
+let hitPosition1
+let currentTime = 60
 
-function randomSquare(){         // creating a whack a mole situation
-    squares.forEach(square => {
-        square.classList.remove('mole')
+// top row
+function randomSquare() {         // creating a whack a mole situation
+    squares.forEach(square => { //for each square in squares
+        square.classList.remove('mole') // remove mole from class name
+        square.classList.add('hole')
     })
-    let randomSquare = squares[Math.floor(Math.random()* 9)]
-    randomSquare.classList.add("mole");
+
+    let randomSquare = squares[Math.floor(Math.random() * 3)] // randomly select square with math
+    console.log(randomSquare)
+  
+    randomSquare.classList.add("mole") // add mole to class
+    randomSquare.classList.remove('hole')
+    hitPosition = randomSquare.id  // var = the mole location
 }
+//bottom row
+function randomSquare1() {         // creating a whack a mole situation for bottom line
+    squares1.forEach(square1 => { //for each square in squares1
+        square1.classList.remove('mole') // remove mole from class name
+        square1.classList.add('hole')
+    })
+
+    let randomSquare1 = squares1[Math.floor(Math.random() * 3)] // randomly select square with math
+    console.log(randomSquare1)
+    randomSquare1.classList.add("mole") // add mole to class
+    randomSquare1.classList.remove("hole")
+
+    hitPosition1 = randomSquare1.id  // var = the mole location
+}
+
+//start game
+randomSquare();
+randomSquare1();
+
+
 
 // adding functionality for clicking on 'mole'
-squares.forEach(id => {
-    square.addEventListener('mousedown' () => {
-        if (square.id == )
-    }
-    )
+squares.forEach(square => {
+    square.addEventListener('mousedown', function (){
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+            randomSquare()
+
+        }
+    })
 })
 
-// function for the 'mole' to move
-function moveMole(){
-    let timerId = null;
-    timerId  = setInterval(randomSquare, 500);
+squares1.forEach(square1 => {
+    square1.addEventListener('mousedown', function (){
+        if (square1.id == hitPosition1) {
+            result++
+            score.textContent = result
+            hitPosition1 = null
+            randomSquare1()
 
+        }
+    })
+})
+
+// // function for the 'mole' to move
+// function moveMole(){
+//      timerId  = setInterval(randomSquare, 1000);
+
+// }
+
+// moveMole()
+
+function countDown(){
+    currentTime-- // subtract -- from currentTime
+    timeLeft.textContent = currentTime // in time-left on html print currentTime
+
+    if (currentTime == 0){ // when game ends
+        clearInterval(countDownTimerId)
+        clearInterval(timerId)
+        alert("Game Over! Your final score is" + result)
+    }
 }
 
-moveMole()
+let countDownTimerId = setInterval(countDown, 1000)
+
+
+//////////xxxxxxx
+//make the mole a celeb
