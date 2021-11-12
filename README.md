7fffa6c95aede4c6748d0df2bdbf476a25fca826
kyleisme14 <KyleKallman@gmail.com>
2021-11-12 15:48:17 -0800

    friday update

--- README.md
+++ README.md
@@ -15,10 +15,19 @@ Final game should look like this.
 
 But first we will do a simple whack-a-mole to get there.
 
-
+wireframe:
 ![Screen Shot 2021-11-08 at 10 34 50 AM](https://user-images.githubusercontent.com/27470736/140798567-5e97740d-5197-49b3-9977-63e5a95306d3.png)
+
+execution:
+![Screen Shot 2021-11-10 at 1 19 23 PM](https://user-images.githubusercontent.com/27470736/141195612-06f035ca-0468-4d82-82be-fddeeca9e1d7.png)
+
 
 Upon thinking about the inherent violence of my original idea, I've decided to try something a little different... without changing the name. 
 
 Now: You are a paparazi trying to take photo's of celebrities. You want HEADSHOTS ONLY. 
+
+nvm I don't like that. I'm going full blown fps. 
+It's gonna feel like a 90s arcade game. 
+
+HeadShots Only: shoot the bad guys. 
 
--- main.js
+++ main.js
@@ -1,14 +1,47 @@
 let squares = document.querySelectorAll(".square")
 const mole = document.querySelectorAll(".mole")
 let squares1 = document.querySelectorAll(".square1")
-const mole1 = document.querySelectorAll(".mole")
+const mole1 = document.querySelectorAll(".mole1")
+let squares2 = document.querySelectorAll(".square2")
+const mole2 = document.querySelectorAll(".mole2")
 const timeLeft = document.querySelector("#time-left")
 const score = document.querySelector("#score")
 let timerId = null
 let result = 0
 let hitPosition
 let hitPosition1
-let currentTime = 60
+let hitPosition2
+let currentTime = 30
+
+
+//maybe instead of whack-a-mole pushing the mole to a random spot, instead
+//it like either is down or up?
+
+
+// setTimeout function to delay pop up
+setTimeout(() => {randomSquare()}, 5000);
+setTimeout(() => {randomSquare()}, 10000);
+setTimeout(() => {randomSquare()}, 15000);
+setTimeout(() => {randomSquare()}, 25000);
+setTimeout(() => {randomSquare1()}, 5000);
+setTimeout(() => {randomSquare1()}, 15000);
+setTimeout(() => {randomSquare1()}, 25000);
+setTimeout(() => {randomSquare2()}, 10000);
+setTimeout(() => {randomSquare2()}, 15000);
+setTimeout(() => {randomSquare2()}, 25000);
+
+
+setTimeout(() => {
+    squares.forEach(square => { 
+        square.classList.remove('mole') 
+        square.classList.add('hole');
+    randomSquare.classList.add("mole") // add mole to class  
+    randomSquare.classList.remove('hole')
+    hitPosition = randomSquare.id 
+})
+}, 5000);
+
+
 
 // top row
 function randomSquare() {         // creating a whack a mole situation
@@ -19,30 +52,41 @@ function randomSquare() {         // creating a whack a mole situation
 
     let randomSquare = squares[Math.floor(Math.random() * 3)] // randomly select square with math
     console.log(randomSquare)
-  
-    randomSquare.classList.add("mole") // add mole to class
+
+    randomSquare.classList.add("mole") // add mole to class  
     randomSquare.classList.remove('hole')
     hitPosition = randomSquare.id  // var = the mole location
 }
-//bottom row
+// 
+//second row
 function randomSquare1() {         // creating a whack a mole situation for bottom line
     squares1.forEach(square1 => { //for each square in squares1
-        square1.classList.remove('mole') // remove mole from class name
+        square1.classList.remove('mole1') // remove mole from class name
         square1.classList.add('hole')
     })
 
     let randomSquare1 = squares1[Math.floor(Math.random() * 3)] // randomly select square with math
     console.log(randomSquare1)
-    randomSquare1.classList.add("mole") // add mole to class
+    randomSquare1.classList.add("mole1") // add mole to class
     randomSquare1.classList.remove("hole")
 
     hitPosition1 = randomSquare1.id  // var = the mole location
 }
+function randomSquare2() {         // creating a whack a mole situation for bottom line
+    squares2.forEach(square1 => { //for each square in squares1
+        square1.classList.remove('mole2') // remove mole from class name
+        square1.classList.add('hole')
+    })
 
+    let randomSquare2 = squares2[Math.floor(Math.random() * 3)] // randomly select square with math
+    console.log(randomSquare1)
+    randomSquare2.classList.add("mole2") // add mole to class
+    randomSquare2.classList.remove("hole")
+
+    hitPosition2 = randomSquare2.id  // var = the mole location
+}
 //start game
 randomSquare();
-randomSquare1();
-
 
 
 // adding functionality for clicking on 'mole'
@@ -53,11 +97,9 @@ squares.forEach(square => {
             score.textContent = result
             hitPosition = null
             randomSquare()
-
         }
     })
 })
-
 squares1.forEach(square1 => {
     square1.addEventListener('mousedown', function (){
         if (square1.id == hitPosition1) {
@@ -65,10 +107,11 @@ squares1.forEach(square1 => {
             score.textContent = result
             hitPosition1 = null
             randomSquare1()
-
         }
     })
 })
+
+
 
 // // function for the 'mole' to move
 // function moveMole(){
@@ -83,14 +126,24 @@ function countDown(){
     timeLeft.textContent = currentTime // in time-left on html print currentTime
 
     if (currentTime == 0){ // when game ends
-        clearInterval(countDownTimerId)
+        clearInterval(countDownTimerId) //clear the timer
         clearInterval(timerId)
-        alert("Game Over! Your final score is" + result)
+        alert("Game Over! Your final score is " + result)
     }
 }
 
 let countDownTimerId = setInterval(countDown, 1000)
 
 
-//////////xxxxxxx
-//make the mole a celeb
+
+// /// hide and reveal start?
+// function myFunction() {
+//     var x = document.getElementById("myDIV");
+//     if (x.style.display === "none") {
+//       x.style.display = "block";
+//     } else {
+//       x.style.display = "none";
+//     }
+//   } 
+
+
