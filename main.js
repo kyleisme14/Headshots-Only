dd781a77bb9979d771627a6eeee9720d11e9e378
kyleisme14 <KyleKallman@gmail.com>
2021-11-15 00:06:31 -0800

    finished version of ghost-fighter

--- main.js
+++ main.js
@@ -12,34 +12,16 @@ let hitPosition
 let hitPosition1
 let hitPosition2
 let currentTime = 30
+let startButton = document.getElementById("myDiv");
+let startPage = document.getElementById("startPage");
+let restartPage = document.getElementById("restartPage");
 
 
 //maybe instead of whack-a-mole pushing the mole to a random spot, instead
 //it like either is down or up?
 
 
-// setTimeout function to delay pop up
-setTimeout(() => {randomSquare()}, 5000);
-setTimeout(() => {randomSquare()}, 10000);
-setTimeout(() => {randomSquare()}, 15000);
-setTimeout(() => {randomSquare()}, 25000);
-setTimeout(() => {randomSquare1()}, 5000);
-setTimeout(() => {randomSquare1()}, 15000);
-setTimeout(() => {randomSquare1()}, 25000);
-setTimeout(() => {randomSquare2()}, 10000);
-setTimeout(() => {randomSquare2()}, 15000);
-setTimeout(() => {randomSquare2()}, 25000);
 
-
-setTimeout(() => {
-    squares.forEach(square => { 
-        square.classList.remove('mole') 
-        square.classList.add('hole');
-    randomSquare.classList.add("mole") // add mole to class  
-    randomSquare.classList.remove('hole')
-    hitPosition = randomSquare.id 
-})
-}, 5000);
 
 
 
@@ -50,7 +32,7 @@ function randomSquare() {         // creating a whack a mole situation
         square.classList.add('hole')
     })
 
-    let randomSquare = squares[Math.floor(Math.random() * 3)] // randomly select square with math
+    let randomSquare = squares[Math.floor(Math.random() * 4)] // randomly select square with math
     console.log(randomSquare)
 
     randomSquare.classList.add("mole") // add mole to class  
@@ -61,37 +43,36 @@ function randomSquare() {         // creating a whack a mole situation
 //second row
 function randomSquare1() {         // creating a whack a mole situation for bottom line
     squares1.forEach(square1 => { //for each square in squares1
-        square1.classList.remove('mole1') // remove mole from class name
+        square1.classList.remove('mole') // remove mole from class name
         square1.classList.add('hole')
     })
 
     let randomSquare1 = squares1[Math.floor(Math.random() * 3)] // randomly select square with math
     console.log(randomSquare1)
-    randomSquare1.classList.add("mole1") // add mole to class
+    randomSquare1.classList.add("mole") // add mole to class
     randomSquare1.classList.remove("hole")
 
     hitPosition1 = randomSquare1.id  // var = the mole location
 }
 function randomSquare2() {         // creating a whack a mole situation for bottom line
     squares2.forEach(square1 => { //for each square in squares1
-        square1.classList.remove('mole2') // remove mole from class name
+        square1.classList.remove('mole') // remove mole from class name
         square1.classList.add('hole')
     })
 
-    let randomSquare2 = squares2[Math.floor(Math.random() * 3)] // randomly select square with math
+    let randomSquare2 = squares2[Math.floor(Math.random() * 2)] // randomly select square with math
     console.log(randomSquare1)
-    randomSquare2.classList.add("mole2") // add mole to class
+    randomSquare2.classList.add("mole") // add mole to class
     randomSquare2.classList.remove("hole")
 
     hitPosition2 = randomSquare2.id  // var = the mole location
 }
-//start game
-randomSquare();
 
 
 // adding functionality for clicking on 'mole'
 squares.forEach(square => {
     square.addEventListener('mousedown', function (){
+        
         if (square.id == hitPosition) {
             result++
             score.textContent = result
@@ -107,6 +88,16 @@ squares1.forEach(square1 => {
             score.textContent = result
             hitPosition1 = null
             randomSquare1()
+        }
+    })
+})
+squares2.forEach(square2 => {
+    square2.addEventListener('mousedown', function (){
+        if (square2.id == hitPosition2) {
+            result++
+            score.textContent = result
+            hitPosition2 = null
+            randomSquare2()
         }
     })
 })
@@ -128,22 +119,46 @@ function countDown(){
     if (currentTime == 0){ // when game ends
         clearInterval(countDownTimerId) //clear the timer
         clearInterval(timerId)
-        alert("Game Over! Your final score is " + result)
+        // change the page to score/restart
+        startButton.style.display = "none";
+        restartPage.style.display = "block";
+        document.querySelector(".endScore").textContent = ("Game Over! Your final score is " + result);
     }
 }
 
-let countDownTimerId = setInterval(countDown, 1000)
+function restart(){
+    // when time = 0
+    // give prompt
+    // score
+    // offer to restart
+}
 
 
 
 // /// hide and reveal start?
-// function myFunction() {
-//     var x = document.getElementById("myDIV");
-//     if (x.style.display === "none") {
-//       x.style.display = "block";
-//     } else {
-//       x.style.display = "none";
-//     }
-//   } 
+function myFunction() {
+    startPage.style.display = "none";
+    restartPage.style.display = "none";
+    startButton.style.display = "block";
+        console.log('playing')
+        let countDownTimerId = setInterval(countDown, 1000)
+
+    //start game
+    randomSquare();
+    // setTimeout function to delay pop up
+    setTimeout(() => {randomSquare()}, 10000);
+    setTimeout(() => {randomSquare()}, 15000);
+    setTimeout(() => {randomSquare()}, 25000);
+    setTimeout(() => {randomSquare()}, 26000);
+    setTimeout(() => {randomSquare()}, 27000);
+    setTimeout(() => {randomSquare1()}, 5000);
+    setTimeout(() => {randomSquare1()}, 15000);
+    setTimeout(() => {randomSquare1()}, 25000);
+    setTimeout(() => {randomSquare2()}, 10010);
+    setTimeout(() => {randomSquare2()}, 12010);
+    setTimeout(() => {randomSquare2()}, 15000);
+    setTimeout(() => {randomSquare2()}, 25000);
+    setTimeout(() => {randomSquare2()}, 28000);
+  } 
 
 
